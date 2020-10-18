de4b7be1534da23c919af704271e8a96bbd5168b
sfazli96 <sfazli96@gmail.com>
2020-10-18 03:35:18 -0700

    Added header title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
+      <Header> </Header>
       <h1>Welcome to our Browser Router Tutorial! Look around.</h1>
     </div>
   );
---
+++ src/Directory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Directory extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This is our Directory </h2>
+            </div>
+        )
+    }
+}
+
+export default Directory;
